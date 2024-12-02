f4547dda3a9a536e095ab9360eb45acd2b9e1b80
MatheusR2t <00001119044650sp@al.educacao.sp.gov.br>
2024-12-02 10:45:05 -0300

    .
    .

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Corinthians',
+    'Qual o camisa 10 do Corinthians?',
+    'Rodrigo Garro'
+)
+
+criaCartao(
+    'SCCP',
+    'Qual foi a data de fundação do Corinthians?',
+    'O Corinthians foi fundado em 1° de setembro de 1910'
+)
+
+criaCartao(
+    'LIBERTADORES',
+    'Em que ano o Corinthians conquistou sua primeira Libertadores?',
+    'Conquistou em 2012'
+)
+
+criaCartao(
+    'CASA DO CORINTHIANS',
+    'Qual é o estádio do Corinthians?',
+    'Neo Química Arena, localizado em Itaquera, São Paulo'
+)
